8c7e3b0a6c6c5d823b177695fbf3d661708f7588
stefanbc <stefcosma@gmail.com>
2016-09-10 00:46:44 +0300

    Major design and theme changes

---
+++ gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt) {
+
+    require('time-grunt')(grunt);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('./package.json'),
+        
+        clean: {
+            build: {
+                src: [
+                    './assets/css/*'
+                ]
+            }
+        },
+        
+        sass: {
+            build: {
+                options: {
+                    style     : 'compressed',
+                    sourcemap : 'none'
+                },
+                files: {
+                    './assets/css/style.min.css' : './assets/scss/index.scss'
+                }
+            }
+        },
+        
+        watch: {
+            options: {
+                atBegin: true
+            },
+            files: [
+                './assets/scss/*.scss'
+            ],
+            tasks: ['clean:build', 'sass:build']
+        }
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    
+    grunt.registerTask('default', ['clean:build', 'sass:build']);
+};
