dc3e5d787a04c8c17948d9bbeffb7194d185e47c
stefanbc <stefcosma@gmail.com>
2019-04-27 22:56:38 +0300

    Adds custom api endpoint and integrates data from endpoint

--- README.md
+++ README.md
@@ -6,11 +6,20 @@ Open source Ghost theme.
 Customization
 --
 
-Make sure you replace the URLs in the `partials/social.hbs` file with your own social media URLs.
+1. Make sure you replace the URLs in the `partials/social.hbs` file with your own social media URLs.
+2. This theme has multiple custom pages: `about`, `projects` and `speaking`. Checkout the Ghost [docs](https://docs.ghost.org/api/handlebars-themes/context/page/#templates) for more info about custom pages. To customize the data, on the right side, in the `projects` and `speaking` pages you'll first need to add this to your `routes.yaml` file, bellow the `routes` key:
+```
+  /custom/api/:
+    permalink: /custom/api/
+    template: api
+    content_type: json
+```
+More info about the `routes.yaml` file [here](https://docs.ghost.org/api/handlebars-themes/routing/).
+After that you can customize the `api.hbs` file with your data, just make sure the structure remains the same.
 
-This theme has multiple custom pages: /about, /projects and /speaking. You should modify the tables in each of the pages. Checkout the Ghost [docs](https://docs.ghost.org/api/handlebars-themes/context/page/#templates) for more info about custom pages.
+3. Additionally you should replace all the image in the `assets/images` folder. You can use [this useful tool](http://realfavicongenerator.net/).
 
-Additionally you should replace all the image in the `assets/images` folder. You can use [this useful tool](http://realfavicongenerator.net/).
+After you've completed the steps above, you can zip the theme and upload it.
 
 This theme is compatible with Ghost 2.x
 
@@ -30,3 +39,4 @@ Available script you can run for development and production:
 * `yarn watch` - will watch for any sass file modifications and will build.
 * `yarn zip` - will zip the theme for production.
 * `yarn validate` - will validate the zip created with the previous command.
+* `yarn deploy` - will build the theme for production, zip and validate.
---
+++ assets/source/index.js
@@ -0,0 +1,55 @@
+export default class Asgar {
+    constructor() {
+        document.addEventListener("DOMContentLoaded", () => {
+
+            let xhr = new XMLHttpRequest(),
+                loading = document.getElementsByClassName("fa-sync")[0],
+                checkURL = () => {
+                    return window.location.href.includes('/projects') || window.location.href.includes('/speaking');
+                }
+
+            xhr.onload = () => {
+
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    loading.style.display = 'none';
+
+                    let data = JSON.parse(xhr.response),
+                        projects = document.getElementsByClassName("projects-list")[0],
+                        events = document.getElementsByClassName("events-list")[0],
+                        addItem = (html, parent) => {
+                            let item = document.createElement("tr");
+                            item.innerHTML = html;
+                            parent.getElementsByTagName('tbody')[0].appendChild(item);
+                        }
+
+                    if(projects) {
+                        data.projects.forEach(object => {
+                            let project = `<td>${object.name}</td><td>${object.description}</td><td class="text-center"><a href="${object.url}" title="Fork ${object.name}" target="_blank"><i class="fas fa-code-branch"></i></a></td>`;
+
+                            addItem(project, projects);
+                        });
+                    }
+
+                    if(events) {
+                        data.events.forEach(object => {
+                            let event = `<td>${object.name}</td><td>${object.year}</td><td class="text-center"><div class="row">${object.slides ? `<div class="one-half column"><a href="${object.slides}" title="Slides"><i class="fas fa-file-powerpoint"></i></a></div>` : ''}${object.video ? `<div class="one-half column"><a href="${object.video}" title="Video"><i class="fas fa-video"></i></a></div>` : ''}</div></td>`;
+
+                            addItem(event, events);
+                        });
+                    }
+                } else {
+                    console.log('Could not connect to the api!');
+                }
+
+            };
+
+            if (checkURL()) {
+                xhr.open('GET', '/custom/api');
+                xhr.send();
+            }
+
+        });
+    }
+}
+
+const run = new Asgar();
